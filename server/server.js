8047fed75171941a770e538382f0eafd7ec0cd0c
Koichiro Yokoi <112148201+unnshoyuukou4515@users.noreply.github.com>
2023-10-26 15:35:48 +0900

    feat:Score model-Controller-server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const cookieParser = require('cookie-parser');
 
-const userController = require("../userController/userController");
 
+const userController = require("./userController/userController");
+const scoreController = require("./scoreController/scoreController");
 
 const PORT = process.env.PORT || 8080;
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
 
 //Login
-app.post("/login", userController,checkUser)
+app.post("/login", userController.checkUser)
+
 //Create New Account
-app.post("/createNewAccount", userController.createNewAccount)
+app.post("/createNewAccount", userController.createNewAccout)
+
+
+//get all scores
+app.get("/score/all", scoreController.getAllScores)
+
+//save Score
+app.post("/score/save", scoreController.saveScore)
+
+//get Rankings
+app.get("/score/ranking", scoreController.getRanking)
