a7f614593784cb0b81c1f84bfbc049ea5e3346ff
John Jenkins <knights70@gmail.com>
2023-10-26 11:46:33 +0900

    feat: Implemented error messages for username and password on LoginForm

--- client/vite-project/src/components/LoginForm.jsx
+++ client/vite-project/src/components/LoginForm.jsx
@@ -4,16 +4,42 @@ import { DevTool } from "@hookform/devtools";
 
 export const LoginForm = () => {
     const form = useForm();
-    const { register, control } = form;
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        console.log("form submitted ", data)
+    }
 
     return (
         <div>
-            <form>
+            <h1>Login Form</h1>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username' {...register("username")} />
+                <input 
+                    type='text' 
+                    id='username' 
+                    {...register("username", {
+                        required: {
+                            value: true,
+                            message: "Username is required."
+                        }
+                    })} 
+                />
+                <p className='error'>{errors.username?.message}</p>
 
                 <label htmlFor='password'>Password</label>
-                <input type='text' id='password' {...register("password")} />
+                <input 
+                    type='text' 
+                    id='password' 
+                    {...register("password", {
+                        required: {
+                            value: true,
+                            message: "Password is required."
+                        }
+                    })} 
+                />
+                <p className='error'>{errors.password?.message}</p>
 
                 <button>Submit</button>
             </form>
