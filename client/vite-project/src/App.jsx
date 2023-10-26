1f9e9202963d238e07125c81e827e3f0fd4ea64c
John Jenkins <knights70@gmail.com>
2023-10-26 15:10:02 +0900

    feat: Implemented RegistrationForm fields

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { LoginForm } from './components/LoginForm'
+import { RegistrationForm } from './components/RegistrationForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <LoginForm />
+      <RegistrationForm />
+      {/* <LoginForm /> */}
     </div>
   )
 }
