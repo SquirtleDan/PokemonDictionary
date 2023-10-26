36c499cb4b2f6211b7f95d44759533e13c51aa19
William Brammer <williamedwardbrammer@gmail.com>
2023-10-26 15:39:03 +0900

    Revert "Login page"

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { LoginForm } from './components/LoginForm'
-import { RegistrationForm } from './components/RegistrationForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <RegistrationForm />
-      {/* <LoginForm /> */}
-    </div>
+    <>
+ POKEMON!!!!!
+    </>
   )
 }
 
