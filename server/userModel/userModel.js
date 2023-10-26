8443a6fcde99b85e923afd534667d9dc90536758
Koichiro Yokoi <112148201+unnshoyuukou4515@users.noreply.github.com>
2023-10-26 12:28:14 +0900

    feat: userController&userModel

---
+++ server/userModel/userModel.js
@@ -0,0 +1,21 @@
+const knex = require("../knex")
+
+//
+//for Log-In
+module.exports ={
+    //Search DB by Username 
+    getDataByUsername(username){
+        return knex('user').where({ username: username }).first()
+    },
+    //Create New Account
+    createUser(username, hash_salted_password,salt,email,first_name,last_name){
+        return knex('user').insert({
+            username:username,
+            hash_salted_password:hash_salted_password,
+            salt:salt,
+            email:email,
+            first_name:first_name,
+            last_name:last_name,
+        })
+    }
+}
