9df9fa92d3e5139c77592cc2000c0b70fbfd6782
John Jenkins <knights70@gmail.com>
2023-10-26 10:45:59 +0900

    feat: Implemented useForm and DevTool for react-hook-form

--- client/vite-project/src/components/LoginForm.jsx
+++ client/vite-project/src/components/LoginForm.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from "@hookform/devtools";
 
 export const LoginForm = () => {
+    const form = useForm();
+    const { register, control } = form;
+
     return (
         <div>
             <form>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username' name='username' />
+                <input type='text' id='username' {...register("username")} />
 
                 <label htmlFor='password'>Password</label>
-                <input type='text' id='password' name='password' />
+                <input type='text' id='password' {...register("password")} />
 
                 <button>Submit</button>
             </form>
+            <DevTool control={control} />
         </div>
     );
 };
