b2298259751f031247ae4790b80885811773211b
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-10-27 12:22:16 +0900

    feat: implement backend for get pokemon, show highest score, register score

---
+++ server/src/score/score-model.js
@@ -0,0 +1,34 @@
+const knex = require("../knex");
+
+//for scoreBoard
+module.exports = {
+  //get all score data(for leaderBoard)
+  getHighestScore(accountId) {
+    return knex
+      .select(
+        knex.raw("MAX(value)"),
+        "game_mode_id"
+      )
+      .from("score")
+      .where({
+        account_id: accountId,
+      })
+      .groupByRaw('game_mode_id')
+      ;
+  },
+  //save score data
+  saveScore: (accountId, gameModeId, value) => {
+    return knex("score").insert({
+      account_id: accountId,
+      game_mode_id: gameModeId,
+      value: value
+    });
+  },
+  //get Ranking(get all data=> sort => get Arr[indexnum]), not yet verifed
+  getRanking: async (value) => {
+    const allScoreData = await knex("score").select("value");
+    const allScoreDataArr = allScoreData.map((eachData) => eachData.value);
+    const ranking = allScoreDataArr.sort((a, b) => b - a);
+    return ranking.indexOf(value) + 1;
+  },
+};
