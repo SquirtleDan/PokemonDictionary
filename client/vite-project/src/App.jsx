976d0a33f4a533aa58e6ee42464aeae4bc88a730
William Brammer <williamedwardbrammer@gmail.com>
2023-10-27 17:47:28 +0900

    fix:match the development branch

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
-// import { LoginForm } from './components/LoginForm'
-// import { RegistrationForm } from './components/RegistrationForm'
-import { Quiz } from './components/Quiz'
+import LoginForm from './components/LoginForm'
+import RegistrationForm from './components/RegistrationForm'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
  
 
   return (
-    <div>
-      <Quiz />
-    </div>
-  )
+    <Router>
+      <Routes>
+      <Route path='/' element={ <LoginForm /> } />
+      <Route path="registration" element={ <RegistrationForm/> } />
+      </Routes>
+  </Router>
+  );
+  
 }
 
 export default App
