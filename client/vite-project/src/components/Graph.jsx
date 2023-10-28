39f088a5066e940d7b43e80b94c5af58c98674fd
William Brammer <williamedwardbrammer@gmail.com>
2023-10-29 01:22:37 +0900

    fix: change version of chart js for deploment

--- client/vite-project/src/components/Graph.jsx
+++ client/vite-project/src/components/Graph.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './Graph.css'
-import { CategoryScale, Chart as ChartJS, LineElement, LinearScale, PointElement } from "chart.js/auto";
+import { CategoryScale, Chart as ChartJS, LineElement, LinearScale, PointElement } from "chart.js";
 import { Line } from "react-chartjs-2";
 
 ChartJS.register(
@@ -21,8 +21,8 @@ export default function Graph() {
             label: 'Rainfall',
             fill: false,
             lineTension: 0.5,
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
+            backgroundColor: 'rgba(255, 203, 5,1)',
+            borderColor: 'rgba(60, 90, 166 ,1)',
             borderWidth: 2,
             data: scores
           }
