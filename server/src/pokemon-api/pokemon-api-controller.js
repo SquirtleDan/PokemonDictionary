c554e5d0284efd80c097d3319a1ad1942c405112
William Brammer <williamedwardbrammer@gmail.com>
2023-10-29 21:49:20 +0900

    fix: change number of pokemon

--- server/src/pokemon-api/pokemon-api-controller.js
+++ server/src/pokemon-api/pokemon-api-controller.js
@@ -4,7 +4,7 @@ module.exports = {
   async insertPokemonDataToDatabase() {
 
     // SETTINGS
-    const noOfPokemon = 1017;
+    const noOfPokemon = 1016;
   
     // SETTING UP POKEMON
     const pokemonData = [];
