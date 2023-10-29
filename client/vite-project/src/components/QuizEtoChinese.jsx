a61ac92a29ce10870f878868507830824194753c
William Brammer <williamedwardbrammer@gmail.com>
2023-10-29 23:39:47 +0900

    fix: remove import files from components

--- client/vite-project/src/components/QuizEtoChinese.jsx
+++ client/vite-project/src/components/QuizEtoChinese.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { Link } from 'react-router-dom';
-
-
 import axios from 'axios'
 import Gameover from './Gameover';
 import Timer from './Timer';
