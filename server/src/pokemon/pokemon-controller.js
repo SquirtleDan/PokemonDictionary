efdd2aef3de211d8b80d4fa91abc38fb0054b52d
Kai Eugene <kai.yougene@gmail.com>
2023-10-31 09:36:48 +0900

    test

--- server/src/pokemon/pokemon-controller.js
+++ server/src/pokemon/pokemon-controller.js
@@ -1,4 +1,4 @@
-const pokemonModel = require('./pokemon-model');
+const pokemonModel = require("./pokemon-model");
 
 module.exports = {
   async getAllPokemon(req, res) {
@@ -7,20 +7,19 @@ module.exports = {
       let amount;
       if (req.query.amount) {
         const specifiedAmount = Number(req.query.amount);
-      
 
         // If query is invalid throw error
-        if (isNaN(specifiedAmount)) throw new Error ('Invalid query');
+        if (isNaN(specifiedAmount)) throw new Error("Invalid query");
         amount = specifiedAmount;
-      } else amount = 100;
-        // assign 100 pokemon if no amount is specified
+      } else amount = 400;
+      // assign 100 pokemon if no amount is specified
 
       // Fetch pokemon data
       const allPokemon = await pokemonModel.getAllPokemon(amount);
-   
+
       res.status(200).send(JSON.stringify(allPokemon));
     } catch (error) {
       res.status(500).send(`Failed to get pokemon: ${error.message}`);
     }
-  }
-}
+  },
+};
