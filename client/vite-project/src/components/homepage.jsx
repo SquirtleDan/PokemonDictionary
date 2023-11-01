1e1f05f3d339d1e14e5a4392672cc8fcf8692769
Kai Eugene <kai.yougene@gmail.com>
2023-11-01 09:20:42 +0900

    FEAT: language selection

--- client/vite-project/src/components/homepage.jsx
+++ client/vite-project/src/components/homepage.jsx
@@ -1,8 +1,8 @@
-import { Link, useNavigate } from 'react-router-dom'
-import './Homepage.css'
-import { useState, useRef } from 'react'
-import Avatar from './Avatar';
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import "./Homepage.css";
+import { useState, useRef } from "react";
+import Avatar from "./Avatar";
+import React from "react";
 
 export default function Homepage(props) {
   const { setLanguageCallback } = props;
@@ -14,86 +14,115 @@ export default function Homepage(props) {
   const navigate = useNavigate();
 
   function handleClick() {
-    setHamburger((prev) => !prev)
+    setHamburger((prev) => !prev);
   }
-  
-return (
+
+  return (
     <>
-    
-    <div className='homedesign'>
-    <br />
-    
-    <Avatar></Avatar>
-    <br />
-    <Link to="/dictionary"><button className='homelink'>Dictionary</button></Link>
-    <img className='home-pic' src="https://i.ibb.co/bN1GVf7/pokedic.png" alt="pokedictionary" />
-    </div>
-    <br />
-    <br />
-    
-    <div className='homedesign'>
+      <div className="homedesign">
+        <br />
 
-    
+        <Avatar></Avatar>
+        <br />
+        <Link to="/dictionary">
+          <button className="homelink">Dictionary</button>
+        </Link>
+        <img
+          className="home-pic"
+          src="https://i.ibb.co/bN1GVf7/pokedic.png"
+          alt="pokedictionary"
+        />
+      </div>
+      <br />
+      <br />
 
-    <Link to='/quiztime' ><button className='homelink'>Timed Quiz</button>
-    <img className='home-pic' src="https://i.ibb.co/ZV0zg0k/for-upload.png" alt="pokedictionary" />
-    </Link>
-    </div>
-    <br />
-    <br />
-   
+      <div className="homedesign">
+        <Link to="/quiztime">
+          <button className="homelink">Timed Quiz</button>
+          <img
+            className="home-pic"
+            src="https://i.ibb.co/ZV0zg0k/for-upload.png"
+            alt="pokedictionary"
+          />
+        </Link>
+      </div>
+      <br />
+      <br />
 
-    {!hamburger ?
-    <div className='homedesign'>
-    <button className='homelink'onClick={handleClick}>Language Quiz</button>
-    <img className='home-pic' src="https://i.ibb.co/ZV0zg0k/for-upload.png" alt="pokedictionary" />
-    </div>
-    :
-    <div className='homedesign'>
-       <label>
-      Select quiz language:
-      <select name="selectedQuiz" ref={quizRef}>
-        <option value="nameJapaneseHrkt">Japanese</option>
-        <option value="nameEnglish">English</option>
-        <option value="nameChineseTraditional">Chinese</option>
-        <option value="nameFrench">French</option>
-        <option value="nameGerman">German</option>
-        <option value="nameSpanish">Spanish</option>
-        <option value="nameItalian">Italian</option>
-      </select>
-    </label>
-    <label>
-      Select answers language:
-      <select name="selectedAnswers" ref={answerRef}>
-        <option value="nameJapaneseHrkt">Japanese</option>
-        <option value="nameEnglish">English</option>
-        <option value="nameChineseTraditional">Chinese</option>
-        <option value="nameFrench">French</option>
-        <option value="nameGerman">German</option>
-        <option value="nameSpanish">Spanish</option>
-        <option value="nameItalian">Italian</option>
-      </select>
-    </label>
-    <button className='homelink' onClick={() => {
-      console.log(setLanguageCallback)
-      setLanguageCallback([quizRef.current.value, answerRef.current.value])
-     // navigate('/quizJ-E');
-      }}>Start</button>
-    <button className='homelinkback' onClick={handleClick}>Close Menu</button>
-    </div>
-    }
+      {!hamburger ? (
+        <div className="homedesign">
+          <button className="homelink" onClick={handleClick}>
+            Language Quiz
+          </button>
+          <img
+            className="home-pic"
+            src="https://i.ibb.co/ZV0zg0k/for-upload.png"
+            alt="pokedictionary"
+          />
+        </div>
+      ) : (
+        <div className="homedesign">
+          <label>
+            Select quiz language:
+            <select name="selectedQuiz" ref={quizRef}>
+              <option value="nameJapaneseHrkt">Japanese</option>
+              <option value="nameEnglish">English</option>
+              <option value="nameChineseTraditional">Chinese</option>
+              <option value="nameFrench">French</option>
+              <option value="nameGerman">German</option>
+              <option value="nameSpanish">Spanish</option>
+              <option value="nameItalian">Italian</option>
+            </select>
+          </label>
+          <label>
+            Select answers language:
+            <select name="selectedAnswers" ref={answerRef}>
+              <option value="nameJapaneseHrkt">Japanese</option>
+              <option value="nameEnglish">English</option>
+              <option value="nameChineseTraditional">Chinese</option>
+              <option value="nameFrench">French</option>
+              <option value="nameGerman">German</option>
+              <option value="nameSpanish">Spanish</option>
+              <option value="nameItalian">Italian</option>
+            </select>
+          </label>
+          <button
+            className="homelink"
+            onClick={() => {
+              console.log(setLanguageCallback);
+              setLanguageCallback([
+                quizRef.current.value,
+                answerRef.current.value,
+              ]);
+              navigate("/quizJ-E");
+            }}
+          >
+            Start
+          </button>
+          <button className="homelinkback" onClick={handleClick}>
+            Close Menu
+          </button>
+        </div>
+      )}
 
+      <br />
+      <br />
+      <Link to="/graph">
+        <button className="homelink">Tracker</button>
+      </Link>
+      <br />
+      <br />
 
-    <br />
-    <br />
-    <Link to="/graph"><button className='homelink'>Tracker</button></Link>
-    <br/>
-    <br />
-    
-    <div className='homedesign'>
-    <Link to="/leaderboard"><button className='homelink'>Leaderboard</button></Link>
-    <img className='home-pic' src="https://i.ibb.co/72wCKVs/highscoreupdated.png" alt="pokedictionary" />
-    </div>
+      <div className="homedesign">
+        <Link to="/leaderboard">
+          <button className="homelink">Leaderboard</button>
+        </Link>
+        <img
+          className="home-pic"
+          src="https://i.ibb.co/72wCKVs/highscoreupdated.png"
+          alt="pokedictionary"
+        />
+      </div>
     </>
-    )
+  );
 }
